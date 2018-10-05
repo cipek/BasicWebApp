f0e2558decd1b0cc6fe9bbc166abde4f4100eaac
Lukasz Niepolski <nipek1@op.pl>
2018-10-05 12:18:24 +0200

    comment

--- src/main/java/com/develogical/WebServer.java
+++ src/main/java/com/develogical/WebServer.java
@@ -12,6 +12,8 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+//My comment
+
 public class WebServer {
 
   public WebServer() throws Exception {
